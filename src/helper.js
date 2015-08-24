02909b3beceb803478e22d0491fb0ee09fbcc893
Manuel Ihl <office@picapipe.com>
2015-08-24 18:45:23 +0200

    Self-Execution Function

--- src/helper.js
+++ src/helper.js
@@ -5,122 +5,125 @@
  * @licence MIT License, http://www.opensource.org/licenses/MIT
  */
 
-angular.module('ambersive.helper',[]);
+(function(window, document, undefined) {'use strict';
 
-angular.module('ambersive.helper').factory('HelperSrv',['$http','$templateCache','$log',
-    function($http,$templateCache,$log){
+    angular.module('ambersive.helper',[]);
 
-        var HelperSrv = {};
+    angular.module('ambersive.helper').factory('HelperSrv',['$http','$templateCache','$log',
+        function($http,$templateCache,$log){
 
-        HelperSrv.config = {
-            'templates':{
-                'useCache':true,
-                'basePath':'',
-                'extension':'.html'
-            }
-        };
+            var HelperSrv = {};
 
-        /**
-         * Function an unifing a response.
-         */
+            HelperSrv.config = {
+                'templates':{
+                    'useCache':true,
+                    'basePath':'',
+                    'extension':'.html'
+                }
+            };
 
-        HelperSrv.response = function(response,callback){
-            if(callback){
-                if(typeof(callback) === 'function'){
-                    callback(response);
+            /**
+             * Function an unifing a response.
+             */
+
+            HelperSrv.response = function(response,callback){
+                if(callback){
+                    if(typeof(callback) === 'function'){
+                        callback(response);
+                    } else {
+                        return response;
+                    }
                 } else {
                     return response;
                 }
-            } else {
-                return response;
-            }
-        };
+            };
 
-        HelperSrv.template = {
-            'get':function(obj,callback){
+            HelperSrv.template = {
+                'get':function(obj,callback){
 
-                if(obj === undefined){
-                    $log.error('The function: HelperSrv.template.get() needs a settings object');
-                    return;
-                }
-
-                var useCache = HelperSrv.config.templates.useCache,
-                    template = null,
-                    templateUrl = null;
-
-                if(angular.isObject(obj) === true){
-                    templateUrl = HelperSrv.template.url(obj);
-                } else {
-                    templateUrl = obj;
-                }
-
-                if(useCache === true){
-                    template = $templateCache.get(templateUrl);
-                    return HelperSrv.response(template, callback);
-                } else {
-                    if(callback === undefined){
-                        $log.error('The function: HelperSrv.template.get() with no use of cache method needs to be called with a callback function. e.g. HelperSrv.template.get(obj,function(html){ });');
+                    if(obj === undefined){
+                        $log.error('The function: HelperSrv.template.get() needs a settings object');
+                        return;
                     }
-                    $http({method: 'GET', url: templateUrl, cache: true}).then(function(result) {
-                        template = result.data;
+
+                    var useCache = HelperSrv.config.templates.useCache,
+                        template = null,
+                        templateUrl = null;
+
+                    if(angular.isObject(obj) === true){
+                        templateUrl = HelperSrv.template.url(obj);
+                    } else {
+                        templateUrl = obj;
+                    }
+
+                    if(useCache === true){
+                        template = $templateCache.get(templateUrl);
                         return HelperSrv.response(template, callback);
-                    });
-                }
-            },
-            'url':function(obj,callback){
-                var templateUrl = null,
-                    init = true;
-                if(obj !== undefined){
+                    } else {
+                        if(callback === undefined){
+                            $log.error('The function: HelperSrv.template.get() with no use of cache method needs to be called with a callback function. e.g. HelperSrv.template.get(obj,function(html){ });');
+                        }
+                        $http({method: 'GET', url: templateUrl, cache: true}).then(function(result) {
+                            template = result.data;
+                            return HelperSrv.response(template, callback);
+                        });
+                    }
+                },
+                'url':function(obj,callback){
+                    var templateUrl = null,
+                        init = true;
+                    if(obj !== undefined){
 
-                    templateUrl = HelperSrv.config.templates.basePath;
+                        templateUrl = HelperSrv.config.templates.basePath;
 
-                    var addToUrl = function(url,init){
-                        if(templateUrl === null || templateUrl === ''){
-                            if(init === true){
-                                templateUrl = url;
+                        var addToUrl = function(url,init){
+                            if(templateUrl === null || templateUrl === ''){
+                                if(init === true){
+                                    templateUrl = url;
+                                } else {
+                                    templateUrl = templateUrl+'/'+url;
+                                }
                             } else {
                                 templateUrl = templateUrl+'/'+url;
                             }
-                        } else {
-                            templateUrl = templateUrl+'/'+url;
-                        }
-                    };
+                        };
 
-                    if(obj.application !== undefined){addToUrl(obj.application,init); init = false;}
-                    if(obj.module !== undefined){addToUrl(obj.module,init); init = false;}
-                    if(obj.folder !== undefined){addToUrl(obj.folder,init); init = false;}
-                    if(obj.template !== undefined){addToUrl(obj.template+HelperSrv.config.templates.extension,init); init= false;}
+                        if(obj.application !== undefined){addToUrl(obj.application,init); init = false;}
+                        if(obj.module !== undefined){addToUrl(obj.module,init); init = false;}
+                        if(obj.folder !== undefined){addToUrl(obj.folder,init); init = false;}
+                        if(obj.template !== undefined){addToUrl(obj.template+HelperSrv.config.templates.extension,init); init= false;}
 
+                    }
+                    return HelperSrv.response(templateUrl, callback);
                 }
-                return HelperSrv.response(templateUrl, callback);
-            }
-        };
+            };
 
-        HelperSrv.generator = {
-            'string':function(len,callback){
-                var lib = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-                return HelperSrv.generator.generate(len,lib,callback);
-            },
-            'number':function(len,callback){
-                var lib = '0123456789';
-                return HelperSrv.generator.generate(len,lib,callback);
-            },
-            'alphanumeric':function(len,callback){
-                var lib = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-                return HelperSrv.generator.generate(len,lib,callback);
-            },
-            'generate':function(length,lib,callback){
-                var text = '';
-                if (length === undefined) { length = 5; }
-                for (var i = 0; i < length; i++) {
-                    text += lib.charAt(Math.floor(Math.random() * lib.length));
-                    if (i + 1 === length) {
-                        return HelperSrv.response(text, callback);
+            HelperSrv.generator = {
+                'string':function(len,callback){
+                    var lib = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+                    return HelperSrv.generator.generate(len,lib,callback);
+                },
+                'number':function(len,callback){
+                    var lib = '0123456789';
+                    return HelperSrv.generator.generate(len,lib,callback);
+                },
+                'alphanumeric':function(len,callback){
+                    var lib = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+                    return HelperSrv.generator.generate(len,lib,callback);
+                },
+                'generate':function(length,lib,callback){
+                    var text = '';
+                    if (length === undefined) { length = 5; }
+                    for (var i = 0; i < length; i++) {
+                        text += lib.charAt(Math.floor(Math.random() * lib.length));
+                        if (i + 1 === length) {
+                            return HelperSrv.response(text, callback);
+                        }
                     }
                 }
-            }
-        };
+            };
 
-        return HelperSrv;
+            return HelperSrv;
 
-}]);
+    }]);
+})(window, document, undefined);
