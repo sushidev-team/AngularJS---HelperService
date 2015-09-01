76d020fe80699b475392381f08d3d89b3444b3ed
Manuel Ihl <office@picapipe.com>
2015-09-01 20:51:37 +0200

    HelperProvider Fix

--- build/helper.js
+++ build/helper.js
@@ -159,23 +159,3 @@
 
     }]);
 })(window, document, undefined);
-/**
- * Helper Service Provider for AngularJS
- * @version v0.0.1
- * @link http://www.ambersive.com
- * @licence MIT License, http://www.opensource.org/licenses/MIT
- */
-
-(function(window, document, undefined) {'use strict';
-    angular.module('ambersive.helper').provider('Helper',['HelperSrv',function(HelperSrv){
-        return {
-            template:HelperSrv.template,
-            response:HelperSrv.response,
-            $get:function(){
-                return {
-                    config:HelperSrv.config
-                };
-            }
-        }
-    }]);
-})(window, document, undefined);
