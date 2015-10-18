7c944f7d2c6d6992c464fa3badd345d17c2a617c
Manuel Ihl <office@picapipe.com>
2015-10-18 19:40:24 +0200

    Add a compile directive

--- readme.md
+++ readme.md
@@ -1,7 +1,7 @@
 # Helper - AngularJS Service
 
 ### Version
-0.0.3
+0.0.4
 
 ### Installation
 
--- src/helper.js
+++ src/helper.js
@@ -179,4 +179,19 @@
 
     }]);
 
+    angular.module('ambersive.helper').directive('compile',['$compile',
+        function($compile){
+            return function(scope,element,attrs){
+                scope.$watch(
+                    function(scope){
+                        return scope.$eval(attrs.compile);
+                    },
+                    function(value){
+                        element.html(value);
+                        $compile(element.contents())(scope);
+                    }
+                );
+            };
+        }]);
+
 })(window, document, undefined);
